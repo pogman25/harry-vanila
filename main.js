7f7b8558e369cd821d02c5649198fd34a7ac7e5c
Alexandr Pogorelov <sapogi21@yandex.ru>
2024-02-14 23:29:19 +0300

    html only

--- main.js
+++ main.js
@@ -1,4 +1,1 @@
 import "./style.css";
-import { getCharacters } from "./scripts/potter.api.js";
-
-document.querySelector("#potter-api").addEventListener("click", getCharacters);
--- scripts/potter.api.js
+++ scripts/potter.api.js
@@ -1,3 +1,9 @@
+export class PotterAPI {
+  constructor(element) {
+
+  }
+}
+
 export async function getCharacters() {
   const rawResult = await fetch("https://api.potterdb.com/v1/characters?page[size]=25&filter[name_cont]=Harry");
   const result = await rawResult.json();
